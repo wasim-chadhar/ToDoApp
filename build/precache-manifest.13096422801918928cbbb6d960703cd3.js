7ae438089a7030a1c8d3c8bef978b8812237709b
Muhammad Wasim Munir <2019cs624@student.uet.edu.pk>
2023-04-21 14:57:39 +0500

    Third commit

---
+++ build/precache-manifest.13096422801918928cbbb6d960703cd3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a81104a5f6777961e24a9df01ef839",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/ToDoApp/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "ce291981a953f6d87e54",
+    "url": "/ToDoApp/static/js/main.948029c5.chunk.js"
+  },
+  {
+    "revision": "292361e27b63ed5a6d18",
+    "url": "/ToDoApp/static/js/runtime-main.2ec72020.js"
+  }
+]);
